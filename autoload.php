be57063a453a689651f0fe5589c17d71c55f6493
Art4es <Art4es@gmail.com>
2019-09-04 18:07:13 +0300

    Add prefix to namespaces

--- autoload.php
+++ autoload.php
@@ -14,7 +14,7 @@
 spl_autoload_register(function ($class) {
 
     // project-specific namespace prefix
-    $prefix = 'Skynet\\';
+    $prefix = 'Skynet';
 
     // base directory for the namespace prefix
     $base_dir = __DIR__;
